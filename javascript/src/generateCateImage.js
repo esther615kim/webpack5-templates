42cc763c220f6cdb44504003235c02419e43a7db
esther615kim <vilchen@naver.com>
2022-04-03 18:38:00 +0900

    add generateCatImage.js

---
+++ javascript/src/generateCateImage.js
@@ -0,0 +1,7 @@
+function generateCateImage() {
+    const catImage1 = [{ "breeds": [], "id": "MTg3NTg2OA", "url": "https://cdn2.thecatapi.com/images/MTg3NTg2OA.jpg", "width": 800, "height": 1066 }];
+
+    return catImage1[0].url;
+}
+
+export default generateCateImage;
---
+++ javascript/src/index.js
@@ -0,0 +1,2 @@
+import generateCateImage
+    from './generateCateImage';
