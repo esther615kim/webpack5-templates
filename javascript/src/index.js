e600fd832ba344e392750e8e54da472852405ab8
esther615kim <vilchen@naver.com>
2022-04-04 02:54:38 +0900

    add devServer options

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,3 +1,4 @@
 import generateCateImage from './generateCateImage';
+import './styles/main.scss'
 
 console.log(generateCateImage());
--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -1,14 +1,46 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
     entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')
+        bundle: path.resolve(__dirname, "src/index.js"),
     },
     output: {
         //folder - can make multiple entry points
-        path: path.resolve(__dirname, 'dist'),
-        //file 
-        filename: '[name].js'
-    }
-}
+        path: path.resolve(__dirname, "dist"),
+        //file
+        filename: "[name][contenthash].js",
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        // open -true open the browser automatically after run dev
+        open: true,
+        // hot - hot reloading
+        hot: true,
+        // compress -enable gzip comression
+        compress: true,
+        // redirect 404 to index.html
+        historyApiFallback: true,
+    },
+    // to handle loaders add module
+    module: {
+        // add rules of array for each loader(or file-type)
+        rules: [
+            {
+                test: /\.scss$/, // any file ends with .scss
+                use: ["style-loader", "css-loader", "sass-loader"], // will use these
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Cat Gallery App",
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+};
