55880e04579130a52e0393be1c78f83d973baece
esther615kim <vilchen@naver.com>
2022-04-10 19:19:32 +0900

    add rules for react and styling files in webpack.config.js

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -8,5 +8,32 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
+    },
+    // webpack 5 feature -live updating
+    devServer: {
+        port: 3010,
+        watchContentBase: true,
+    },
+    //
+    module: {
+        rules: [
+            // handle react files
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modues/,
+                // use - what to do with the js/jsx files
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            // handle styling files
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"],
+            },
+        ]
     }
 }
